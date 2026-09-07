b96dce52c56c4bb101bd1d6c6f5d7f01fbbe06e7
a <102840087+kingdudely@users.noreply.github.com>
2026-09-07 00:31:08 +0000

    Add minimal WebSocket send API

--- tiny-webrtc/include/rtc/platform/WebSocket.h
+++ tiny-webrtc/include/rtc/platform/WebSocket.h
@@ -1,4 +1,5 @@
 #pragma once
+
 #include <cstdint>
 #include <functional>
 #include <span>
@@ -11,10 +12,17 @@ public:
     using BinaryHandler = std::function<void(std::span<const std::uint8_t>)>;
     using CloseHandler = std::function<void()>;
 
+    WebSocketServer() = default;
+    ~WebSocketServer();
+
+    WebSocketServer(const WebSocketServer&) = delete;
+    WebSocketServer& operator=(const WebSocketServer&) = delete;
+
     bool listen(std::uint16_t port, const char* token);
     void poll(int timeoutMs);
     void close();
     bool connected() const noexcept;
+    bool sendBinary(std::span<const std::uint8_t> data);
 
     void setHandlers(OpenHandler, BinaryHandler, CloseHandler);
 

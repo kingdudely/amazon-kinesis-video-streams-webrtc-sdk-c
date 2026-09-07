942eeeaf21145a285facbb0d38b06c3602440023
a <102840087+kingdudely@users.noreply.github.com>
2026-09-07 00:30:18 +0000

    Implement compact STUN interface

--- tiny-webrtc/include/rtc/ice/Stun.h
+++ tiny-webrtc/include/rtc/ice/Stun.h
@@ -1,17 +1,36 @@
 #pragma once
+
+#include <array>
 #include <cstddef>
 #include <cstdint>
+#include <span>
 
 namespace tinyrtc {
 
-bool stunBindingRequest(const std::uint8_t* data, std::size_t size);
-
 struct StunAddress {
-    std::uint8_t address[16]{};
+    std::array<std::uint8_t, 16> address{};
     std::uint16_t port{};
     bool v6{};
 };
 
-bool parseXorMappedAddress(const std::uint8_t* data, std::size_t size, StunAddress& out);
+constexpr std::uint32_t STUN_MAGIC_COOKIE = 0x2112A442u;
+constexpr std::size_t STUN_HEADER_SIZE = 20;
+
+bool isStunPacket(std::span<const std::uint8_t> data) noexcept;
+bool isBindingRequest(std::span<const std::uint8_t> data) noexcept;
+
+bool buildBindingRequest(std::array<std::uint8_t, 256>& out,
+                         std::size_t& size,
+                         const std::uint8_t transactionId[12],
+                         const char* username,
+                         const char* password) noexcept;
+
+bool buildBindingResponse(std::array<std::uint8_t, 256>& out,
+                          std::size_t& size,
+                          const std::uint8_t transactionId[12],
+                          std::uint32_t ip,
+                          std::uint16_t port) noexcept;
+
+bool parseXorMappedAddress(std::span<const std::uint8_t> data, StunAddress& out) noexcept;
 
 }
